89a28c38807abc6aac64ab57cbb75e9199a11881
Tremerel <84023901+Tremerel@users.noreply.github.com>
2021-06-23 10:35:47 +0200

    change text shadow

--- js/mouseShadow.js
+++ js/mouseShadow.js
@@ -4,13 +4,15 @@ const walk = 50
 
 function shadow(e){
   const {offsetWidth: width, offsetHeight: height} = hero
-  let {offsetX: x, offsetY: y} = e
+  // let {offsetX: x, offsetY: y} = e
 
-  if(this !== e.target){
-    x = x + e.target.offsetleft
-    y = y + e.target.offsetTop
+  // if(this !== e.target){
+  //   x = x + e.target.offsetleft
+  //   y = y + e.target.offsetTop
 
-  }
+  // }
+  let { clientX: x, clientY: y } = e;
+
   const xWalk = Math.round((x/width*walk)- (walk/2))
   const yWalk = Math.round((y/height*walk)- (walk/2))
 
